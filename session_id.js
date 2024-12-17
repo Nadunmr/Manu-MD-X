411080669fdc5d8bf784d8c5b84df45a51b56107
Nadunmr <133474808+Nadunmr@users.noreply.github.com>
2024-12-17 13:38:46 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Manu-MD&eM1mjYrT#817pVJqiV3uiD2ZMizTPrhCF_1q6FCH-jStOJ1mMH9A",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&PJNjiAYQ#xSFt-fC4E1PwQNw_L4B3jz4J6VbIQ50QbLsY4ywcjSI",
 };
